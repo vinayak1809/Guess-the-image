cd66165f5f5e673cc1edaf769a8b128288e91c1a
vinayak1809 <vinayakrshinde18@gmail.com>
2023-09-07 00:04:49 +0530

    styling of image file

--- frontend/src/Page/Image.jsx
+++ frontend/src/Page/Image.jsx
@@ -16,27 +16,30 @@ const Image = () => {
     }
 
 
-  const handleImageChange = (e) => {
-    const selectedFile = e.target.files[0];
-    
-    if (selectedFile && selectedFile.type.startsWith('image/')) {
-        setImageSrc(selectedFile);
-    } else {
-      alert('Please select a valid image file.');
-        setImageSrc(null);
-    }
-  };
+    const handleImageChange = (e) => {
+        const selectedFile = e.target.files[0];
+        
+        if (selectedFile && selectedFile.type.startsWith('image/')) {
+            setImageSrc(selectedFile);
+        } else {
+        alert('Please select a valid image file.');
+            setImageSrc(null);
+        }
+    };
 
     return (
         <div className="hello">
-            <div>{imageSrc ? <img src={URL.createObjectURL(imageSrc)} alt="Image" />: <>not found</> }</div>
-            <div>
-                <input type="file" accept="image/*" onChange={(e) => handleImageChange(e)}></input>
+            <div className="img">{imageSrc ? <img srcSet={URL.createObjectURL(imageSrc)} sizes="(max-width:600px) 480px,800px" alt="Image" />: <>not found</> }</div>
+            <div className="upload-check">
+                <div className="upload-btn-wrapper">
+                    <button class="btn">Upload a file</button>
+                    <input type="file" accept="image/*" onChange={(e) => handleImageChange(e)}></input>
+                </div>
+                <div>
+                    <button className="btn" onClick={()=> check()}>Check the image</button>
+                </div>
             </div>
-            <div>
-                <button onClick={()=> check()}>Check the image</button>
-            </div>
-            <div>
+            <div className="prediction">
                 <h3>Prediction is : {imgName}</h3>
             </div>
         </div>
