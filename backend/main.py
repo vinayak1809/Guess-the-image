97bb86a4c0dac43043bc9e609ff97899a0ebabe0
vinayak1809 <vinayakrshinde18@gmail.com>
2023-12-23 13:58:03 +0530

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,6 @@ def checkImage(image):
     (training_images, training_labels), (testing_images,
                                          testing_labels) = datasets.cifar10.load_data()
 
-    print("Checking image")
     training_images, testing_images = training_images / 255, testing_images / 255
 
     class_names = ['Plane', 'Car', 'Bird', 'Cat',
